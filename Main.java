016a8a6255f5741f5a1181229c5be1bd45b3564c
balez4464 <balez4464@wrdsb.ca>
2020-10-06 15:15:44 +0000

    created a program that allows user to find thier factorial

--- Main.java
+++ Main.java
@@ -1,6 +1,8 @@
+import java.util.Scanner;
+
 /**
- *
- * @author 
+ *This program will calculate the factorial of the usersnumber
+ * @author Zachary Balean
  */
 public class Main {
 
@@ -9,7 +11,28 @@ public class Main {
    * @param args the command line arguments
    */
   public static void main(String[] args) {
-    // TODO code application logic here
-    
+    // creates Scanner
+    Scanner input = new Scanner(System.in);
+
+    //asks user for number
+    System.out.println("Please enter an integer to calculate the factorial of");
+
+    //creates integers for the factorial
+    int usersNumber = input.nextInt();
+    //creates an interger for the product
+    int product = 1;
+
+    //creaetes accumulator and for loop
+    for(int accumulator = usersNumber - 1; accumulator >= 1; accumulator--){
+      //finds product for the first time
+      if(accumulator == usersNumber - 1){
+        product = usersNumber * accumulator;
+        //calculates product for reaminaing numbers
+      }else{
+        product = product * accumulator;
+      }
+    }
+    //prints factorial
+    System.out.print(usersNumber + "! = " + product);
   }
 }
